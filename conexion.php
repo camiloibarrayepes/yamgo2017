bcb3f2bdc051e76305be622d04cf5597a54d6c9b
System Administrator <root@MacBook-Pro-de-camilo.local>
2017-05-06 13:01:46 -0500

    cambio de base de datos temporal a yamgo2

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 	define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'yamgo');
+    define('DB_DATABASE', 'yamgo2');
 
 
     $link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
